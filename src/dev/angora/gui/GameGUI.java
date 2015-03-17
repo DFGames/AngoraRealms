cd67578c0ef5eb14b18b54b69d310b23153b8835
Alex <alexander.richard96@gmail.com>
2015-03-16 23:08:10 -0300

    Got images working.

--- src/dev/angora/gui/GameGUI.java
+++ src/dev/angora/gui/GameGUI.java
@@ -1,31 +1,57 @@
 package dev.angora.gui;
 
 import java.awt.Button;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
-public class GameGUI extends JFrame implements ActionListener {
-	
-	public Button drawCard = new Button("Draw Card");
-	
-	public void createGui(JFrame p) {
-		
-		drawCard.addActionListener(this);
-		drawCard.setBounds(20,30,80,30);
-		p.add(drawCard);
-		
-	}
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class GameGUI extends JPanel implements ActionListener {
+
+    public Button drawCard = new Button("Draw Card");
+    BufferedImage image;
+    
+    public GameGUI() {
+        drawCard.addActionListener(this);
+        add(drawCard);
+    }
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 
 		Object cause = event.getSource();
-		
+
 		if (cause == drawCard) {
-		System.out.println("Ay");
-		
+			System.out.println("Ay");
 		}
 	}
-	
+    
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        
+        Image img; 
+        try 
+        {
+        img = ImageIO.read(getClass().getResource("/dev/angora/images/plains/Cheetah.png"));
+        g.drawImage(img, 0, 0, 450, 700, this);
+        } 
+        catch (IOException e) 
+        {
+        e.printStackTrace();
+        }
+    }
 }
--- src/dev/angora/main/Main.java
+++ src/dev/angora/main/Main.java
@@ -1,14 +1,28 @@
 package dev.angora.main;
 
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
-public class Main extends JPanel {
+import dev.angora.gui.GameGUI;
+
+public class Main {
+	
+	public static JFrame p = new JFrame("Angora Realms");
 	
 	public static void main (String[] args) {
 		
-		Launch pnlAngora = new Launch();
-		pnlAngora.createScreen();
+		new Main();
+		
 	}
 	
+    public Main() {
+
+        JFrame p = new JFrame("Angora Realms");
+        p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GameGUI g = new GameGUI();
+        p.add(g);
+        p.setLocationRelativeTo(null);
+        p.pack();
+        p.setVisible(true);
+
+    }
 } 
